257510ff38a112a9a44614c8067b1b039b99d593
pharmarin <marin_maxfresh-and-good@hotmail.fr>
2019-02-28 11:23:52 +0100

    Fixed margin header Fixed smaller Card header in Produits

--- archive.php
+++ archive.php
@@ -11,7 +11,7 @@
       <div id="content" role="main">
         <header class="mb-4 border-bottom">
           <h1>
-            <?php echo get_taxonomy(get_query_var( 'taxonomy' ))->label ?> : 
+            <?php echo get_taxonomy(get_query_var( 'taxonomy' ))->label ?> :
             <?php
               $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
               $parent = get_term_by( 'id', $term->parent, get_query_var( 'taxonomy' ) );
@@ -23,7 +23,9 @@
           </h1>
         </header>
         <div class="row">
-          <?php get_template_part('loops/index-loop'); ?>
+          <?php if(have_posts()): while(have_posts()): the_post();?>
+            <?php get_template_part('loops/index-post'); ?>
+          <?php endwhile; endif; ?>
         </div>
       </div><!-- /#content -->
     </div>
--- loops/abstract-phytotherapie.php
+++ loops/abstract-phytotherapie.php
@@ -12,13 +12,13 @@ The Abstract Aromatherapie
         <div class="card mb-4">
           <header class="card-header">
             <?php echo get_the_post_thumbnail(get_the_ID(), [68, 68], ['class' => 'border rounded float-right']) ?>
-            <h1 class="mb-0">
+            <h5 class="mb-0">
               <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-            </h1>
-            <h2 class="text-muted font-italic mb-0">
+            </h5>
+            <h6 class="text-muted font-italic mb-0">
               <?php $loop = new PharmaLoop_aromatherapie(get_the_content());
               echo $loop->get_the_subtitle(); ?>
-            </h2>
+            </h6>
           </header>
           <div class="card-body">
             <main>
